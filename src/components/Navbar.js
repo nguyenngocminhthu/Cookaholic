b0c027769aab05561602935f38f97ca9d985e060
nguyenngocminhthu <67693186+nguyenngocminhthu@users.noreply.github.com>
2021-10-31 16:14:03 +0700

    Login SignUp

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,11 +5,21 @@ import { ReactComponent as Logo } from "../img/logo.svg";
 import "../css/navbar.css";
 import { NavLink } from "react-router-dom";
 import Search from "./search";
+import Modal from 'react-awesome-modal';
+import "../css/auth.css";
 
 const Navbar = () => {
     const [click, setClick] = useState(false);
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
+
+    const [visible, setVisible] = useState(false);
+    const openModal = () => setVisible(!visible);
+    const closeModal = () => setVisible(false);
+
+    const [signup, setSignUp] = useState(false);
+    const openSignUp = () => setSignUp(!signup);
+    const closeSignUp = () => setSignUp(false);
 
     return (
         <div className="nav">
@@ -45,27 +55,28 @@ const Navbar = () => {
                     <Search />
 
                     <li className="option mobile-option" onClick={closeMobileMenu}>
-                        <NavLink className="aStyle" to="/about">
+                        <p className="aStyle" value="Open" onClick={openModal}>
                             SIGN IN
-                        </NavLink>
+                        </p>
                     </li>
                     <li className="option mobile-option" onClick={closeMobileMenu}>
-                        <NavLink className="aStyle" to="/about">
+                        <p className="aStyle" value="Open" onClick={openSignUp}>
                             SIGN UP
-                        </NavLink>
+                        </p>
                     </li>
                 </ul>
+
             </div>
             <ul className="signin-up">
                 <li className="sign-in" onClick={closeMobileMenu}>
-                    <NavLink className="aStyle" to="/about">
+                    <p className="aStyle" value="Open" onClick={openModal}>
                         SIGN IN
-                    </NavLink>
+                    </p>
                 </li>
-                <li onClick={closeMobileMenu}>
-                    <NavLink className="aStyle" to="/about">
+                <li className="sign-up" onClick={closeMobileMenu}>
+                    <p className="aStyle" value="Open" onClick={openSignUp}>
                         SIGN UP
-                    </NavLink>
+                    </p>
                 </li>
             </ul>
             <div className="mobile-menu" onClick={handleClick}>
@@ -75,6 +86,84 @@ const Navbar = () => {
                     <MenuIcon className="menu-icon" />
                 )}
             </div>
+            <Modal
+                visible={visible}
+                width="90%"
+                height="90%"
+                effect="fadeInUp"
+                onClickAway={closeModal}
+            >
+                <div>
+                    <div className="close-detail">
+                        <a href="javascript:void(0);" onClick={closeModal}><i className="fa fa-times" aria-hidden="true"></i></a>
+                    </div>
+
+                    <div>
+                        <h3>Sign In</h3>
+                        <div className="form-group">
+                            <label>Email address</label>
+                            <input type="email" className="form-control" placeholder="Enter email" />
+                        </div>
+
+                        <div className="form-group">
+                            <label>Password</label>
+                            <input type="password" className="form-control" placeholder="Enter password" />
+                        </div>
+
+                        <div className="form-group">
+                            <div>
+                                <input type="checkbox" id="customCheck1" />
+                                <label htmlFor="customCheck1">Remember me</label>
+                            </div>
+                        </div>
+
+                        <button type="submit">Submit</button>
+                        <p className="forgot-password text-right">
+                            Forgot <a href="#">password?</a>
+                        </p>
+                    </div>
+                </div>
+
+            </Modal>
+            <Modal
+                visible={signup}
+                width="90%"
+                height="90%"
+                effect="fadeInUp"
+                onClickAway={closeSignUp}
+            >
+                <div>
+                    <div className="close-detail">
+                        <a href="javascript:void(0);" onClick={closeSignUp}><i className="fa fa-times" aria-hidden="true"></i></a>
+                    </div>
+
+                    <div>
+                        <h3>Sign Up</h3>
+                        <div className="form-group">
+                            <label>Email address</label>
+                            <input type="email" className="form-control" placeholder="Enter email" />
+                        </div>
+
+                        <div className="form-group">
+                            <label>Password</label>
+                            <input type="password" className="form-control" placeholder="Enter password" />
+                        </div>
+
+                        <div className="form-group">
+                            <div>
+                                <input type="checkbox" id="customCheck1" />
+                                <label htmlFor="customCheck1">Remember me</label>
+                            </div>
+                        </div>
+
+                        <button type="submit">Submit</button>
+                        <p className="forgot-password text-right">
+                            Forgot <a href="#">password?</a>
+                        </p>
+                    </div>
+                </div>
+
+            </Modal>
         </div>
     );
 };
